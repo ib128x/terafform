b3ed64c3812bd52fd378a809d6b74cf7ffdc3a07
Bristman, Israel (ib128x) <ib128x@intl.att.com>
2019-08-06 17:18:30 +0300

    Initial commit

--- connections.tf
+++ connections.tf
@@ -1,3 +1,13 @@
+provider "google" {
+	credentials = "${file("../account.json")}"
+	project = "thrashingcorecode"
+	region = "us-west1"
+}
+
+provider "aws" {
+	region = "us-west-2"
+}
+
 provider "azurerm" {
 	subscription_id = "0"
 	client_id = "1"
