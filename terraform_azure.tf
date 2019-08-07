6e49a2dc6f8791a6dcd3be221baace5dd2616d0d
Bristman, Israel (ib128x) <ib128x@intl.att.com>
2019-08-07 18:03:09 +0300

    Initial commit

--- terraform_azure.tf
+++ terraform_azure.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 
 # Create a resource group if it doesn’t exist
 resource "azurerm_resource_group" "terraformgroup" {
-    name     = "RG-CentralUS"
+    name     = "RG-NGEAG-CentralUS"
     location = "centralus"
 
     tags = {
@@ -68,27 +68,28 @@ resource "azurerm_network_security_group" "terraformnsg" {
     }
 }
 
-# Create network interface
-#resource "azurerm_network_interface" "terraformnic" {
-#    name                      = "NGEAG-NIC"
-#    location                  = "centralus"
-#    resource_group_name       = "${azurerm_resource_group.terraformgroup.name}"
-#    network_security_group_id = "${azurerm_network_security_group.terraformnsg.id}"
-#
-#    ip_configuration {
-#        name                          = "NGEAG-NicConfiguration"
-#        subnet_id                     = "${azurerm_subnet.terraformsubnet.id}"
-#        private_ip_address_allocation = "Dynamic"
-#    }
-#
-#    tags = {
-#        environment = "ngeag"
-#    }
-#}
 
 # Create network interface
 resource "azurerm_network_interface" "terraformnic" {
-    name                      = "NGEAG-PUB-NIC"
+    name                      = "NGEAG-NIC"
+    location                  = "centralus"
+    resource_group_name       = "${azurerm_resource_group.terraformgroup.name}"
+    network_security_group_id = "${azurerm_network_security_group.terraformnsg.id}"
+
+    ip_configuration {
+        name                          = "NGEAG-NicConfiguration"
+        subnet_id                     = "${azurerm_subnet.terraformsubnet.id}"
+        private_ip_address_allocation = "Dynamic"
+        # public_ip_address_id          = "${azurerm_public_ip.myterraformpublicip.id}"
+    }
+
+    tags = {
+        environment = "ngeag"
+    }
+}
+
+resource "azurerm_network_interface" "terraformnicPub" {
+    name                      = "NGEAG-NIC-PUB"
     location                  = "centralus"
     resource_group_name       = "${azurerm_resource_group.terraformgroup.name}"
     network_security_group_id = "${azurerm_network_security_group.terraformnsg.id}"
@@ -104,7 +105,6 @@ resource "azurerm_network_interface" "terraformnic" {
         environment = "ngeag"
     }
 }
-
 
 
 # Generate random text for a unique storage account name
@@ -130,17 +130,16 @@ resource "azurerm_storage_account" "storageaccount" {
     }
 }
 
-
 # Create Jump-Server VM
-resource "azurerm_virtual_machine" "terraformvm" {
-    name                  = "ngeagJumpServer"
+resource "azurerm_virtual_machine" "terraformvm1" {
+    name                  = "ngeagJumpServer1"
     location              = "centralus"
     resource_group_name   = "${azurerm_resource_group.terraformgroup.name}"
-    network_interface_ids = ["${azurerm_network_interface.terraformnic.id}"]
-    vm_size               = "Standard_DS1_v2"
+    network_interface_ids = ["${azurerm_network_interface.terraformnicPub.id}"]
+	vm_size               = "Standard_DS1_v2"
 
     storage_os_disk {
-        name              = "OsDisk"
+        name              = "OsDiskJump"
         caching           = "ReadWrite"
         create_option     = "FromImage"
         managed_disk_type = "Premium_LRS"
@@ -154,7 +153,7 @@ resource "azurerm_virtual_machine" "terraformvm" {
     }
 
     os_profile {
-        computer_name  = "vm"
+        computer_name  = "ngeagJumpServer"
         admin_username = "ngeag"
 		admin_password = "ngeagAa1234%^!"
     }
@@ -172,7 +171,6 @@ resource "azurerm_virtual_machine" "terraformvm" {
         environment = "centralus"
     }
 }
-
 
 # Create virtual machine
 resource "azurerm_virtual_machine" "terraformvm" {
@@ -187,6 +185,7 @@ resource "azurerm_virtual_machine" "terraformvm" {
         caching           = "ReadWrite"
         create_option     = "FromImage"
         managed_disk_type = "Premium_LRS"
+		disk_size_gb	  = "512"
     }
 
     storage_image_reference {
@@ -216,7 +215,123 @@ resource "azurerm_virtual_machine" "terraformvm" {
     }
 }
 
+## scaleset ##
+##############
+
+
+# Create public IPs
+resource "azurerm_public_ip" "vmssterraformpublicip" {
+    name                         = "PublicIPVMSS"
+    location                     = "centralus"
+    resource_group_name          = "${azurerm_resource_group.terraformgroup.name}"
+    allocation_method            = "Dynamic"
+
+    tags = {
+        environment = "ngeag"
+    }
+}
+
+
+resource "azurerm_lb" "vmss" {
+ name                = "vmss-lb"
+ location            = "centralus"
+ resource_group_name = "${azurerm_resource_group.terraformgroup.name}"
+
+ frontend_ip_configuration {
+   name                 = "PublicIPVMSS"
+   public_ip_address_id = "${azurerm_public_ip.vmssterraformpublicip.id}"
+ }
+
+ tags = {
+     environment = "centralus"
+ }
+}
+
+resource "azurerm_lb_backend_address_pool" "bpepool" {
+ resource_group_name = "${azurerm_resource_group.terraformgroup.name}"
+ loadbalancer_id     = "${azurerm_lb.vmss.id}"
+ name                = "BackEndAddressPool"
+}
+
+resource "azurerm_lb_probe" "vmss" {
+ resource_group_name = "${azurerm_resource_group.terraformgroup.name}"
+ loadbalancer_id     = "${azurerm_lb.vmss.id}"
+ name                = "ssh-running-probe"
+ port                = "8082"
+}
+
+resource "azurerm_lb_rule" "lbnatrule" {
+   resource_group_name            = "${azurerm_resource_group.terraformgroup.name}"
+   loadbalancer_id                = "${azurerm_lb.vmss.id}"
+   name                           = "http"
+   protocol                       = "Tcp"
+   frontend_port                  = "8083"
+   backend_port                   = "8084"
+   backend_address_pool_id        = "${azurerm_lb_backend_address_pool.bpepool.id}"
+   frontend_ip_configuration_name = "PublicIPVMSS"
+   probe_id                       = "${azurerm_lb_probe.vmss.id}"
+}
+
+resource "azurerm_virtual_machine_scale_set" "vmss" {
+ name                = "vmscaleset"
+ location            = "centralus"
+ resource_group_name = "${azurerm_resource_group.terraformgroup.name}"
+ upgrade_policy_mode = "Manual"
+
+ sku {
+   name     = "Standard_DS1_v2"
+   tier     = "Standard"
+   capacity = 4
+ }
+
+ storage_profile_image_reference {
+   publisher = "Canonical"
+   offer     = "UbuntuServer"
+   sku       = "16.04-LTS"
+   version   = "latest"
+ }
+
+ storage_profile_os_disk {
+   name              = ""
+   caching           = "ReadWrite"
+   create_option     = "FromImage"
+   managed_disk_type = "Standard_LRS"
+ }
 
 
 
+ storage_profile_data_disk {
+   lun          = 0
+   caching        = "ReadWrite"
+   create_option  = "Empty"
+   disk_size_gb   = 512
+ }
+
+ os_profile {
+   computer_name_prefix = "vmlab"
+   admin_username       = "ngeag"
+   admin_password       = "ngeagAa1234%^!"
+#   custom_data          = "${file("web.conf")}"
+ }
+
+ os_profile_linux_config {
+   disable_password_authentication = false
+ }
+
+ network_profile {
+   name    = "terraformnetworkprofile"
+   primary = true
+
+   ip_configuration {
+     name                                   = "IPConfiguration"
+     subnet_id                              = "${azurerm_subnet.terraformsubnet.id}"
+     load_balancer_backend_address_pool_ids = ["${azurerm_lb_backend_address_pool.bpepool.id}"]
+     primary = true
+   }
+ }
+
+ tags = {
+     environment = "centralus"
+ }
+}
 
